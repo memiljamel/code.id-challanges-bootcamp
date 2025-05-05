f8b8b5e37d5fcd7da5c594fae0974034d995e3b7
Emil Jamel Mahmuda <memiljamel@gmail.com>
2025-05-05 09:05:04 +0700

    add validation annotation

--- 03 - Spring MVC/eshooper-web/src/main/java/com/codeid/eshopper/entities/Shipper.java
+++ 03 - Spring MVC/eshooper-web/src/main/java/com/codeid/eshopper/entities/Shipper.java
@@ -1,6 +1,10 @@
 package com.codeid.eshopper.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "shippers", schema = "oe")
@@ -8,18 +12,28 @@ public class Shipper {
 
     @Id
     @Column(name = "shipper_id")
+    @NotNull
+    @PositiveOrZero
     private Long shipperId;
 
     @Column(name = "company_name")
+    @NotBlank
+    @Size(min = 3, max = 40)
     private String companyName;
 
     @Column(name = "phone")
+    @NotBlank
+    @Size(min = 8, max = 24)
     private String phone;
 
     public Shipper() {
     }
 
-    public Shipper(Long shipperId, String companyName, String phone) {
+    public Shipper(
+            @NotNull @PositiveOrZero Long shipperId,
+            @NotBlank @Size(min = 3, max = 40) String companyName,
+            @NotBlank @Size(min = 3, max = 24) String phone
+    ) {
         this.shipperId = shipperId;
         this.companyName = companyName;
         this.phone = phone;
